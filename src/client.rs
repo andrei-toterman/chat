6312a23bb272f3edd0b07d873fd4d7ce3e1d3702
Andrei Toterman <andrei.toterman@gmail.com>
2021-08-16 19:28:25 +0300

    replaced custom bincode codec with tokio-serde

--- src/client.rs
+++ src/client.rs
@@ -9,22 +9,28 @@ pub enum Message {
 
 #[tokio::test]
 async fn test_message_codec() {
-    use crate::BincodeCodec;
     use futures::{SinkExt, StreamExt};
-    use tokio_util::codec::{FramedRead, FramedWrite};
+    use tokio_serde::{formats::SymmetricalBincode, SymmetricallyFramed};
+    use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 
     let message1_send = Message::Join("username".to_owned());
     let message2_send = Message::Say("something".to_owned());
     let message3_send = Message::Leave;
 
     let buffer = Vec::new();
-    let mut frame_writer = FramedWrite::new(buffer, BincodeCodec::new());
+    let mut frame_writer = SymmetricallyFramed::new(
+        FramedWrite::new(buffer, LengthDelimitedCodec::new()),
+        SymmetricalBincode::default(),
+    );
     frame_writer.send(message1_send.clone()).await.unwrap();
     frame_writer.send(message2_send.clone()).await.unwrap();
     frame_writer.send(message3_send.clone()).await.unwrap();
 
-    let buffer = frame_writer.into_inner();
-    let mut frame_reader = FramedRead::new(buffer.as_slice(), BincodeCodec::new());
+    let buffer = frame_writer.into_inner().into_inner();
+    let mut frame_reader = SymmetricallyFramed::new(
+        FramedRead::new(buffer.as_slice(), LengthDelimitedCodec::new()),
+        SymmetricalBincode::default(),
+    );
     let message1_receive = frame_reader.next().await.unwrap().unwrap();
     let message2_receive = frame_reader.next().await.unwrap().unwrap();
     let message3_receive = frame_reader.next().await.unwrap().unwrap();
