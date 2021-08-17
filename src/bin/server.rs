dc7641be58403fcb3ff7b8c307d5ef203733a401
Andrei Toterman <andrei.toterman@gmail.com>
2021-08-17 18:42:02 +0300

    added basic server executable

---
+++ src/bin/server.rs
@@ -0,0 +1,23 @@
+use std::sync::Arc;
+
+use tokio::net::TcpListener;
+
+use chat::server::{handle_client, State};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = std::env::args().nth(1).expect("Usage: server <address>");
+    let listener = TcpListener::bind(address).await?;
+    let state = Arc::new(State::default());
+
+    loop {
+        let (stream, address) = listener.accept().await?;
+        let state = state.clone();
+        tokio::spawn(async move {
+            match handle_client(state, stream, address).await {
+                Ok(_) => println!("client {} terminated successfully", address),
+                Err(err) => eprintln!("client {} failed: {:?}", address, err),
+            }
+        });
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,19 @@
-use std::sync::Arc;
+use std::{collections::HashSet, net::SocketAddr, sync::Arc};
 
+use futures::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
+use tokio::{
+    net::TcpStream,
+    sync::{
+        broadcast::{self, error::RecvError},
+        RwLock,
+    },
+};
+use tokio_serde::{formats::SymmetricalBincode, SymmetricallyFramed};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+
+use crate::client;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum Message {
@@ -19,4 +31,115 @@ pub enum Error {
     Internal,
     #[error("lost {0} messages due to connection issues")]
     Lost(u64),
+}
+
+pub struct State {
+    usernames: RwLock<HashSet<Arc<String>>>,
+    sender: broadcast::Sender<Message>,
+}
+
+impl Default for State {
+    fn default() -> Self {
+        let (sender, _) = broadcast::channel(64);
+        Self {
+            usernames: Default::default(),
+            sender,
+        }
+    }
+}
+
+impl State {
+    fn broadcast(&self, message: Message) {
+        let _ = self.sender.send(message);
+    }
+}
+
+pub async fn handle_client(
+    state: Arc<State>,
+    stream: TcpStream,
+    address: SocketAddr,
+) -> std::io::Result<()> {
+    let (reader, writer) = stream.into_split();
+    let mut from_client = SymmetricallyFramed::new(
+        FramedRead::new(reader, LengthDelimitedCodec::new()),
+        SymmetricalBincode::default(),
+    );
+    let mut to_client = SymmetricallyFramed::new(
+        FramedWrite::new(writer, LengthDelimitedCodec::new()),
+        SymmetricalBincode::default(),
+    );
+
+    // loop label is here only because my IDE has a bug and it needs it
+    let username = 'username: loop {
+        match from_client.next().await {
+            None => return Ok(()),
+            Some(result) => match result {
+                Ok(client::Message::Join(username)) => {
+                    let username = Arc::new(username);
+                    if state.usernames.write().await.insert(username.clone()) {
+                        break 'username username;
+                    }
+                    to_client.send(Message::Err(Error::UsernameTaken)).await?;
+                }
+                Err(error) => {
+                    eprintln!("client {} erred asking for username: {:?}", address, error);
+                    to_client.send(Message::Err(Error::Internal)).await?;
+                }
+                _ => {}
+            },
+        }
+    };
+
+    // everything is in one big block so that i can run some additional code no matter how it ends
+    // bootleg defer
+    let result: std::io::Result<()> = async {
+        let mut receiver = state.sender.subscribe();
+        state.broadcast(Message::Joined(username.clone()));
+        println!("client {} joined as {}", address, username);
+
+        loop {
+            tokio::select! {
+                result = receiver.recv() => match result {
+                    Ok(message) => {
+                        if matches!(
+                            message,
+                            Message::Said(_, _) | Message::Joined(_) | Message::Left(_)
+                        ) {
+                            to_client.send(message).await?;
+                        }
+                    }
+                    Err(RecvError::Lagged(lost)) => {
+                        to_client.send(Message::Err(Error::Lost(lost))).await?;
+                    }
+                    _ => {}
+                },
+                option = from_client.next() => match option {
+                    None => break,
+                    Some(result) => match result {
+                        Ok(message) => match message {
+                            client::Message::Leave => break,
+                            client::Message::Say(what) => {
+                                let what = Arc::new(what);
+                                state.broadcast(Message::Said(username.clone(), what.clone()));
+                                println!("client {} aka {} said: {}", address, username, what);
+                            }
+                            _ => {}
+                        },
+                        Err(error) => {
+                            eprintln!("client {} erred asking for message: {:?}", address,  error);
+                            to_client.send(Message::Err(Error::Internal)).await?;
+                        }
+                    },
+                }
+            }
+        }
+        Ok(())
+    }
+    .await;
+
+    state.broadcast(Message::Left(username.clone()));
+    state.usernames.write().await.remove(&username);
+    println!("client {} aka {} left", address, username);
+
+    result
 }
