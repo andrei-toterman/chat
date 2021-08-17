d268b2b3a9ba6c86d2e22bded11dc0d084bf78d2
Andrei Toterman <andrei.toterman@gmail.com>
2021-08-17 16:58:06 +0300

    added server errors

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,22 @@
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
 pub enum Message {
     Joined(Arc<String>),
     Said(Arc<String>, Arc<String>),
     Left(Arc<String>),
+    Err(Error),
+}
+
+#[derive(Debug, Error, Serialize, Deserialize, Clone, Eq, PartialEq)]
+pub enum Error {
+    #[error("that username is already taken")]
+    UsernameTaken,
+    #[error("internal server error")]
+    Internal,
+    #[error("lost {0} messages due to connection issues")]
+    Lost(u64),
 }
